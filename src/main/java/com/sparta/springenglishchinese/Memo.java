38753a3c8652c2af375796e8d1fc029d0a7c0538
MyYeonbi <yeonbi_kim@icloud.com>
2025-04-21 20:29:00 +0900

    Feat (memo) : Applied multiple Lombok annotations to auto-generate constructors and accessors(应用多个 Lombok 注解以自动生成构造器和访问器)

--- src/main/java/com/sparta/springenglishchinese/Memo.java
+++ src/main/java/com/sparta/springenglishchinese/Memo.java
@@ -1,6 +1,17 @@
 package com.sparta.springenglishchinese;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@RequiredArgsConstructor
 public class Memo {
 
   private String username;
@@ -8,18 +19,13 @@ public class Memo {
   private String contents;
 
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
 
-  public String getUsername() {
-    return this.username;
-  }
 }
 
 class Main {
   public static void main(String[] args) {
     Memo memo = new Memo();
+
     memo.setUsername("Yeonbi");
     System.out.println(memo.getUsername());
   }
