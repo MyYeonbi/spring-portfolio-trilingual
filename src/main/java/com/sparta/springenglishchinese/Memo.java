dfec805cd0973d0a081bd00791d3e3d3dd319a2a
MyYeonbi <yeonbi_kim@icloud.com>
2025-04-21 20:16:34 +0900

    Feat(memo) : Manually implemented and tested getter/setter methods(手动实现并测试 getter/setter 方法)

--- src/main/java/com/sparta/springenglishchinese/Memo.java
+++ src/main/java/com/sparta/springenglishchinese/Memo.java
@@ -1,13 +1,26 @@
 package com.sparta.springenglishchinese;
 
 
-import lombok.Getter;
-
-@Getter
 public class Memo {
 
   private String username;
 
   private String contents;
 
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Memo memo = new Memo();
+    memo.setUsername("Yeonbi");
+    System.out.println(memo.getUsername());
+  }
 }
