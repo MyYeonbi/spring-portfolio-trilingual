2018eab5f2c83767fbe94048adcd4c41f2384ef8
MyYeonbi <yeonbi_kim@icloud.com>
2025-04-21 16:14:09 +0900

    chore(init): Initialized Spring Boot project with basic structure(初始化 Spring Boot 项目和基础结构)

---
+++ src/main/java/com/sparta/springenglishchinese/SpringEnglishChineseApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springenglishchinese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEnglishChineseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringEnglishChineseApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sparta/springenglishchinese/SpringEnglishChineseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springenglishchinese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEnglishChineseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
