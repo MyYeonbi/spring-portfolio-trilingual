1aeee510456bb97915a7af054d1ba75394f39e70
MyYeonbi <yeonbi_kim@icloud.com>
2025-04-22 21:47:08 +0900

    Feat (controller) : Used @ResponseBody to return raw string instead of  resolving to a view (使用 @ResponseBody 直接返回字符串而不是解析为视图)
    - Added @ResponseBody to prevent Spring from resolving the return value as a view name.
    Thie ensures that the raw string "Hello World !" is returned as the HTTP response body.
    
    - 添加 @ResponseBody 注解，避免 Spring 将返回值解析为视图名称。
    这样可以直接将 "Hello World!" 字符串作为响应体返回。

--- src/main/java/com/sparta/springenglishchinese/controller/HelloController.java
+++ src/main/java/com/sparta/springenglishchinese/controller/HelloController.java
@@ -2,13 +2,15 @@ package com.sparta.springenglishchinese.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
   @GetMapping("/api/hello")
+  @ResponseBody
   public String hello() {
     return
-        "Hello World!";
+        "Hello World";
   }
 
 
