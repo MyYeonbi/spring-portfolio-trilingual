dded38862b66beb8c098cacb4d47dd3bd0808ef1
MyYeonbi <yeonbi_kim@icloud.com>
2025-04-22 21:29:35 +0900

    Feat (controller): Tested view resolution via String return in @Controller (通过 @Controller 的 String 返回值测试视图解析机制)
    In a @Controller class, returning a String will trigger Spring to resolve it as a view name and  look for a corresponding HTML file under / resources/templates.
    
    在 @Controller 中返回 String 时，Spring 会将其解析为视图名称，并在 /resources/templates 目录下寻找对应的 HTML 文件。

--- src/main/java/com/sparta/springenglishchinese/calculator/Memo.java
+++ src/main/java/com/sparta/springenglishchinese/calculator/Memo.java
@@ -1,4 +1,4 @@
-package com.sparta.springenglishchinese;
+package com.sparta.springenglishchinese.calculator;
 
 
 import lombok.AllArgsConstructor;
--- src/main/java/com/sparta/springenglishchinese/controller/HelloController.java
+++ src/main/java/com/sparta/springenglishchinese/controller/HelloController.java
@@ -1,15 +1,15 @@
 package com.sparta.springenglishchinese.controller;
 
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
-
   @GetMapping("/api/hello")
   public String hello() {
-    return "Hello World";
+    return
+        "Hello World!";
   }
+
 
 }
