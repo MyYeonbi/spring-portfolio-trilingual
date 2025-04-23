b3822a4355683f70d57560aeeb351d259da1f463
MyYeonbi <yeonbi_kim@icloud.com>
2025-04-24 00:22:36 +0900

    Feat (response) : Returned manual JSON string using @ResponseBody for basic API response test(使用 @ResponseBody 手动返回 JSON 字符串以测试基本 API 响应)

---
+++ src/main/java/com/sparta/springenglishchinese/response/ResponseController.java
@@ -0,0 +1,21 @@
+package com.sparta.springenglishchinese.response;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/response")
+public class ResponseController {
+
+  // Content-Type: text/html
+  // Response Body
+  // {"name":"yeonbi","age":30}
+  @GetMapping("/json/string")
+  @ResponseBody
+  public String helloStringJson() {
+    return "{\"name\":\"yeonbi\",\"age\":30}";
+  }
+}
