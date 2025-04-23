e1f0c081016a19fbef71c004bc76bf21da56ae57
MyYeonbi <yeonbi_kim@icloud.com>
2025-04-23 10:47:09 +0900

    Fix (view) : Removed controller mapping for static HTML and accessed directly via /hello.html (移除静态 HTML 的控制器映射，通过 /hello.html 直接访问)
    - When placing an HTML file in /static, it should be accessed via URL like /hello.html.
    Using a controller with @GetMapping to return the static file as a view will result a template resolution error.
    
    - 如果将 HTML 文件放在 /static 目录中，应通过 /hello.html URL 直接访问。
    如果通过 @GetMapping 返回该文件，会因为模板解析失败而报错。

---
+++ src/main/java/com/sparta/springenglishchinese/html/HtmlController.java
@@ -0,0 +1,15 @@
+package com.sparta.springenglishchinese.html;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HtmlController {
+
+  @GetMapping("/static-hello")
+  public String hello() {
+    return "hello.html";
+  }
+
+}
