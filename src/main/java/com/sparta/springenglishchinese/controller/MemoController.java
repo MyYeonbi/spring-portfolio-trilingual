5a0b63ee283851db721dc62a0391b13833bd4723
MyYeonbi <yeonbi_kim@icloud.com>
2025-04-27 23:18:48 +0900

    Feat (memory-db) : Added in-memory storage for Memo objects using HashMap (使用 HashMap 实现 Memo 对象的内存存储)
     - Implemented a basic in-memory database using HashMap<Long, Memo>
    - Dynamically generates a unique ID for each new memo
    - Stores memos temoraily without using a real database.
    - Prepares for CRUD functionality expansion.
    
    - 使用 HashMap<Long, Memo> 实现了一个基础的内存数据库
    - 为每个新建的 Memo 动态生成唯一的 ID
    - 在没有真正数据库的情况下暂时保存 Memo 数据
    - 为后续扩展 CRUD 功能做准备

--- src/main/java/com/sparta/springenglishchinese/controller/MemoController.java
+++ src/main/java/com/sparta/springenglishchinese/controller/MemoController.java
@@ -4,6 +4,9 @@ package com.sparta.springenglishchinese.controller;
 import com.sparta.springenglishchinese.dto.MemoRequestDto;
 import com.sparta.springenglishchinese.dto.MemoResponseDto;
 import com.sparta.springenglishchinese.entity.Memo;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MemoController {
 
+
+  private final Map<Long, Memo> memoList = new HashMap<>();
   @PostMapping("/memos")
   public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto){
     // RequestDto -> Entity
     Memo memo = new Memo( requestDto);
+
+
+     // Memo Max ID Check
+    Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+    memo.setId(maxId);
+    
+     // DB 저장
+    memoList.put(memo.getId(), memo);
+
+    // Entity - > Response Dto
+
   }
+
 
 }
