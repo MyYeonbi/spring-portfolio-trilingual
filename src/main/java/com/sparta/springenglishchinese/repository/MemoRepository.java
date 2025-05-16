3fc428d18818ec0b32b47b4f9e0c4f4ae1275adc
MyYeonbi <yeonbi_kim@icloud.com>
2025-05-16 22:57:22 +0900

    Feat (repository) : add query method to sort memos by modifiedAt descending. (新增按修改时间倒序排序的备忘录查询方法)
    - Added 'findAllByOrderByModifiedAtDesc()' to MemoRepository.
    - This method returns a list of memos sorted by their last modified time in descending order.
    - Enables display of most recently updated memos at the top in API responses.
    
    - 在 MemoRepository 中添加了 `findAllByOrderByModifiedAtDesc()` 方法。
    - 查询结果按 modifiedAt 字段倒序排列。
    - 可用于 API 返回最新更新的备忘录列表。

--- src/main/java/com/sparta/springenglishchinese/repository/MemoRepository.java
+++ src/main/java/com/sparta/springenglishchinese/repository/MemoRepository.java
@@ -1,10 +1,13 @@
 package com.sparta.springenglishchinese.repository;
 
 import com.sparta.springenglishchinese.entity.Memo;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+  List<Memo> findAllByOrderByModifiedAtDesc();
+  
 
 }
